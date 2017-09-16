04712372d5f729c0f339d3ac06f9a18ad3975d5b
Liviu <Liviu@The>
2017-09-16 17:04:43 +0300

    This is my first program in Java.

---
+++ Java6LiviuPopa/src/firstprogram/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * My first program wirtten in Java.
+ */
+package firstprogram;
+
+/**
+ *
+ * @author Emi
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Say hello world    
+        System.out.println("Hello World");
+    }
+}
+    
+
